9e27e8ef7d47714a260e41fb772f9676b8a1b002
XuanDong <22130043@st.hcmuaf.edu.vn>
2026-02-12 14:54:48 +0700

    Update Login New

--- tests/implementing/newapproach/login-new.test.ts
+++ tests/implementing/newapproach/login-new.test.ts
@@ -9,52 +9,58 @@ import { tests } from "../../../utils/helpers/localeTest";
 
 test.describe('Login with normal-email', () => {
     test(`
-        1. Login page is displayed - Login Page shows correctly
-        2. Login with valid email and password - MyPage is displayed
+        1. Login page is displayed correctly
+        2. User can login with valid username and password
         `, async ({ basicAuthPage }) => {
         const homePage = createHomePage(basicAuthPage);
         const loginPage = createLoginPage(basicAuthPage);
         const myPage = new MyPage(basicAuthPage);
 
-        await step("Go to login page", async () => {
+        await step("[STEP] From Homepage, click Logic/Signup icon", async () => {
             await homePage.goToLoginRegisterPage();
         });
 
-        await step("Verify - 1. Login page is displayed - Login Page shows correctly", async () => {
+        await step("[STEP] Verify - 1. Login page is displayed", async () => {
             await loginPage.assertEqual(await loginPage.isLoginPageDisplayed(), true, "Login page is displayed");
             await screenshotAndAttach(basicAuthPage, './screenshots/Login-normal-email', '01 - Login Page')
         });
 
-        await step("Login with valid email and password", async () => {
+        await step("[STEP] Type valid email and passsword into login form and click Sign In button", async () => {
             await loginPage.login(Config.credentials.username, Config.credentials.password);
         });
 
-        await step("Verify - 2. Login with valid email and password - MyPage is displayed", async () => {
+        await step("[STEP] Verify - 2. Login sucess and MyPage is displayed", async () => {
             await myPage.assertEqual(await myPage.isMyPageDisplayed(), true, "Mypage is displayed");
             await screenshotAndAttach(basicAuthPage, './screenshots/Login-normal-email', '02 - MyPage')
         });
     });
-});
 
-test.describe("Login by Facebook login", () => {
-    tests([], `
-        1. Login success by Facebook account - MyPage is displayed
-        `, async ({ basicAuthPage }) => {
-
-        const loginPage = createLoginPage(basicAuthPage);
-        const myPage = new MyPage(basicAuthPage);
-
-        await step("Go to login page", async () => {
-            await loginPage.goToLoginRegisterPage();
-        });
-
-        await step("Login By Facebook account", async () => {
-            await loginPage.loginByFacebookAccount(Config.credentials.fb_username, Config.credentials.fb_password);
-        });
-
-        await step("Verify - 1. Login success by Facebook account - MyPage is displayed", async () => {
-            await myPage.assertEqual(await myPage.isMyPageDisplayed(), true, "Mypage is displayed");
-            await screenshotAndAttach(basicAuthPage, './screenshots/Login-with-facebook', '01 - MyPage')
+    test.afterEach(async ({ basicAuthPage }) => {
+        await step('[STEP] [FINAL STATE]', async () => {
+            await screenshotAndAttach(basicAuthPage, './screenshots/Login-normal-email', 'Final State');
         });
     });
 });
+
+// test.describe("Login by Facebook login", () => {
+//     tests([], `
+//         1. Login success by Facebook account - MyPage is displayed
+//         `, async ({ basicAuthPage }) => {
+
+//         const loginPage = createLoginPage(basicAuthPage);
+//         const myPage = new MyPage(basicAuthPage);
+
+//         await step("Go to login page", async () => {
+//             await loginPage.goToLoginRegisterPage();
+//         });
+
+//         await step("Login By Facebook account", async () => {
+//             await loginPage.loginByFacebookAccount(Config.credentials.fb_username, Config.credentials.fb_password);
+//         });
+
+//         await step("Verify - 1. Login success by Facebook account - MyPage is displayed", async () => {
+//             await myPage.assertEqual(await myPage.isMyPageDisplayed(), true, "Mypage is displayed");
+//             await screenshotAndAttach(basicAuthPage, './screenshots/Login-with-facebook', '01 - MyPage')
+//         });
+//     });
+// });
